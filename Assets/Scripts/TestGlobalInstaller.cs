5af63a47799125958b6df223149ec20a2fc04a30
Elliott Armstrong <KandyMan1990@users.noreply.github.com>
2026-08-15 22:17:36 +0100

    Initial start on including battle module

--- Assets/Scripts/TestGlobalInstaller.cs
+++ Assets/Scripts/TestGlobalInstaller.cs
@@ -1,17 +1,23 @@
 using RPGFramework.Audio;
 using RPGFramework.Audio.Music;
 using RPGFramework.Audio.Sfx;
+using RPGFramework.Battle;
+using RPGFramework.Battle.SharedTypes;
+using RPGFramework.Battle.SharedTypes.Providers;
 using RPGFramework.Core;
 using RPGFramework.Core.Audio;
+using RPGFramework.Core.Databases;
 using RPGFramework.Core.Dialogue.UI;
 using RPGFramework.Core.Rendering;
 using RPGFramework.Core.SaveData;
 using RPGFramework.DI;
 using RPGFramework.Field;
 using RPGFramework.Field.SharedTypes;
+using RPGFramework.Field.SharedTypes.Providers;
 using RPGFramework.Localisation;
 using RPGFramework.Menu;
 using RPGFramework.Menu.SharedTypes;
+using RPGFramework.Menu.SharedTypes.Providers;
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.Rendering.Universal;
@@ -22,18 +28,25 @@ namespace Test
     {
         [SerializeField]
         private MusicAssetProvider m_MusicProvider;
+
         [SerializeField]
         private AudioMixerGroup[] m_MusicMixerGroups;
+
         [SerializeField]
         private SfxAssetProvider m_SfxProvider;
+
         [SerializeField]
         private AudioMixerGroup[] m_SfxMixerGroups;
+
         [SerializeField]
         private DialogueWindowUiProvider m_DialogueWindowUiProvider;
+
         [SerializeField]
         private MemoryServiceArgs m_MemoryServiceArgs;
+
         [SerializeField]
         private UniversalRendererData m_UniversalRendererData;
+
         [SerializeField]
         private ScreenFadeServiceConfig m_ScreenFadeServiceConfig;
 
@@ -53,20 +66,29 @@ namespace Test
 
             IRendererDataProvider rendererDataProvider = new RendererDataProvider(m_UniversalRendererData);
             container.BindSingletonFromInstance(rendererDataProvider);
-            
+
             container.BindSingletonFromInstance<IScreenFadeServiceConfig>(m_ScreenFadeServiceConfig);
 
-            container.BindSingleton<IMenuTypeProvider, MenuTypeProvider>();
+            container.BindSingleton<IBattleArgsProvider, BattleArgsProvider>();
+            container.BindSingleton<IFieldArgsProvider, FieldArgsProvider>();
+            container.BindSingleton<IMenuArgsProvider, MenuArgsProvider>();
+
             container.BindSingleton<IMenuModule, MenuModule>();
             container.BindSingleton<IFieldModule, FieldModule>();
             container.BindSingletonFromInstance<IAudioIntentPlayer>(new GameAudioIntentPlayer(sfxPlayer, GameAudioIntentMaps.Default));
 
             container.BindSingleton<ISaveFactory, SaveFactory>();
-            container.BindSingleton<IModuleResumeMap, ModuleResumeMap>();
+            container.BindSingleton<IModuleDatabase, ModuleDatabase>();
+            container.BindSingleton<IFieldResumeDataStore, FieldResumeDataStore>();
 
             container.BindSingletonFromInstance<IDialogueWindowUiProvider>(m_DialogueWindowUiProvider);
 
             container.BindSingletonFromInstance<IMemoryServiceArgs>(m_MemoryServiceArgs);
+
+            container.BindSingleton<IBattleModule, BattleModule>();
+            container.BindSingleton<IBattleCompleteStateProvider, BattleCompleteStateProvider>();
+
+            container.BindSingleton<ISceneDatabase, SceneDatabase>();
         }
 
         public override void Bootstrap(IDIResolver resolver)
---
+++ Assets/Test/Battle/BattleSceneInstaller.cs
@@ -0,0 +1,24 @@
+using RPGFramework.Battle;
+using RPGFramework.Battle.Databases;
+using RPGFramework.Battle.Loaders;
+using RPGFramework.Battle.Providers;
+using RPGFramework.DI;
+using UnityEngine;
+
+namespace Test.Battle
+{
+    public class BattleSceneInstaller : SceneInstallerBase
+    {
+        [SerializeField]
+        private BattleAudioProvider m_AudioProvider;
+
+        public override void InstallBindings(IDIContainer container)
+        {
+            container.BindSingletonFromInstance<IBattleAudioProvider>(m_AudioProvider);
+
+            container.BindSingleton<IBattleArenaDatabase, BattleArenaDatabase>();
+            container.BindSingleton<IBattleArenaLoader, BattleArenaLoader>();
+            container.BindSingleton<IBattleArenaPresentation, BattleArenaPresentation>();
+        }
+    }
+}
